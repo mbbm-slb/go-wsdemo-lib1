fafbc8a048d7b37acb66356008bb59c483152d96
slb <CSchellenberger@MuellerBBM-vas.de>
2025-05-06 16:06:58 +0200

    use lib2 from lib1

--- text.go
+++ text.go
@@ -1,5 +1,15 @@
 package lib1
 
+import (
+	"fmt"
+
+	"mbbm-vas.com/go/wsdemo/lib2"
+)
+
 func Text() string {
   return "workspace demo"
+}
+
+func TextFromLib2() string {
+  return fmt.Sprintf("lib2 says: %d", lib2.Number())
 }
